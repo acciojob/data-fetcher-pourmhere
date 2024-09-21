7ad22510618798be40747950aae1f6cb474855dc
POURMHERE <pourmhere@gmail.com>
2024-09-21 18:54:31 +0530

    ddd

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,46 @@
 import React from "react";
 import "./../styles/App.css";
 
-const App = () => {
-  const [data, setData] = useState([]);
+const App = () =>  const [error, setError] = useState(null);
+const [isLoaded, setIsLoaded] = useState(false);
+const [items, setItems] = useState([]);
 
-  useEffect(() => {
-    fetch("https://dummyjson.com/products")
-      .then((res) => res.json())
-      .then((result) => {
-        setData(result);
-        console.log(result);
-      });
-  }, []);
+// Note: the empty deps array [] means
+// this useEffect will run once
+// similar to componentDidMount()
+useEffect(() => {
+  fetch("https://dummyjson.com/products")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        setIsLoaded(true);
+        setItems(result);
+      },
+      // Note: it's important to handle errors here
+      // instead of a catch() block so that we don't swallow
+      // exceptions from actual bugs in components.
+      (error) => {
+        setIsLoaded(true);
+        setError(error);
+      }
+    )
+}, [])
 
+if (error) {
+  return <div>Error: {error.message}</div>;
+} else if (!isLoaded) {
+  return <div>Loading...</div>;
+} else {
   return (
-    <div>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </div>
+    <ul>
+      {items.map(item => (
+        <li key={item.id}>
+          {item.name} {item.price}
+        </li>
+      ))}
+    </ul>
   );
-};
+}
+}
 
 export default App;
