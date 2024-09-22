aff05c9ab2ffd0ee9801b63415359f679af74209
POURMHERE <pourmhere@gmail.com>
2024-09-22 18:00:36 +0530

    dd

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,48 @@
 import React, { useEffect, useState } from "react";
+import './../styles/App.css';
 
 const App = () => {
+  const [text, setText] = useState(null);
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState([]);
 
-  // Note: the empty deps array [] means
-  // this useEffect will run once
-  // similar to componentDidMount()
   useEffect(() => {
     fetch("https://dummyjson.com/products")
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setItems(result);
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
         }
-      );
+        return response.json();
+      })
+      .then((json) => {
+        setText(json);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoading(false);
+      });
   }, []);
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
-    return <div>Loading...</div>;
-  } else {
-    return (
-      <div>
-        <pre>{JSON.stringify(items, null, 2)}</pre>
-      </div>
-    );
+  if (loading) {
+    return <h1>Loading...</h1>;
   }
+
+  if (error) {
+    return <h1>An error occurred: {error}</h1>;
+  }
+
+  return (
+    <div>
+      <h1>
+      Data Fetched from API
+      </h1>
+
+      <pre>
+        {JSON.stringify(text, null, 2)}
+      </pre>
+    </div>
+  );
 };
 
 export default App;
